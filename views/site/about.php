b56b0547dc973e6f6bacec29d73daadebfc13360
dfmmalaw <dfmmalaw@gmail.com>
2015-11-13 19:23:50 +0000

    added content to about view

--- views/site/about.php
+++ views/site/about.php
@@ -4,15 +4,24 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'About Me';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        Hi! My name is Dean Friedland and this site is about me and my journey of 
+        learning how to code. I have an incredible wife (Ashley), daughter (Reese), 
+        and son on the way. There is nothing better than being a husband and father. 
+        I love all things martial arts and will train and compete in Brazilian 
+        Jiu Jitsu until the day I die! I also love to surf and have water in my 
+        blood. I am currently living in Georgia and the closest beach is hours 
+        away which I hate. I am originally from South Africa but have lived in the 
+        US since I was a kid after a six year stint in Canada. I have also lived
+        for few months in both Ecuador and Costa Rica (both of which have killer
+        waves) and am blessed to have traveled to many other countries in this amazing
+        world. 
     </p>
 
-    <code><?= __FILE__ ?></code>
 </div>
