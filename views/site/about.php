ef6d9efde63407deb724f0052727d3a5313c76ac
dfmmalaw <dfmmalaw@gmail.com>
2015-11-16 21:40:35 +0000

    altered view formattting

--- views/site/about.php
+++ views/site/about.php
@@ -10,14 +10,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
+    <p> 
         Hi! My name is Dean Friedland and this site is about me and my journey of 
         learning how to code. I have an incredible wife (Ashley), daughter (Reese), 
         and son on the way. There is nothing better than being a husband and father. 
         I love all things martial arts and will train and compete in Brazilian 
         Jiu Jitsu until the day I die! I also love to surf and have water in my 
         blood. I am currently living in Georgia and the closest beach is hours 
-        away which I hate. I am originally from South Africa but have lived in the 
+        way which I hate.
+    </p>
+    <p>
+        I am originally from South Africa but have lived in the 
         US since I was a kid after a six year stint in Canada. I have also lived
         for few months in both Ecuador and Costa Rica (both of which have killer
         waves) and am blessed to have traveled to many other countries in this amazing
--- views/site/story.php
+++ views/site/story.php
@@ -12,26 +12,29 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         My story may not be much different from others that have found themselves 
-        falling into the world of "coding". At 32 I stil wasn't sure what I wanted 
-        do with the rest of my life. Being a husband, father and provider this was 
-        not a great feeling and I knew there had to be something that I would be happy 
-        doing on a daily basis and that would allow me to be financially secure. 
-        Enter programming!
+        falling into the world of "coding". At 32 I still wasn't sure what I wanted 
+        do with the rest of my life. Being a husband, father and provider made  
+        this feeling of uncertainty unbearable and I knew there had to be something 
+        that I would be happy doing on a daily basis and that would allow me to 
+        be financially secure. Enter programming! 
+    </p>
         
-        I finished law school a few years ago and did my clinic at the prosecutor's
+     <p>I finished law school a few years ago and did my clinic at the prosecutor's
         office in Atlanta, GA. I absolutely loved being in the court room and because 
         of something called the Third Year Practice Act I was able to try my own cases
         during my time there. It was awesome to say the least. My plan was to take 
         the bar and become a prosecutor. 
+    </p>
         
-        Fast forward I graduated from law school and tool a job teaching and being
+    <p> Fast forward I graduated from law school and tool a job teaching and being
         the program chair of a criminal justice program at a local college. I was
         there for a little over a year until I was offered a position as an Academic
         Dean at another post-secondary proprtietary school. It was then that I 
         realized that management in education was not what I wanted to do the rest 
         of my life. 
+    </p>
         
-        I ended up leaving Georgia with my wife and three month old daughter and
+    <p> I ended up leaving Georgia with my wife and three month old daughter and
         heading to Florida to take a position with a family member's start-up 
         software company. I immediately fell in love with the tech industry. Everything
         from the technical aspects of the software development life cycle to the 
@@ -41,15 +44,17 @@ $this->params['breadcrumbs'][] = $this->title;
         away by the designers and developers and what they were able to create. I 
         knew immediately that I would have to learn how to code if I was ever 
         going to really understand the technical side of things. 
+    </p>
         
-        I took a remote position with a software development company as a project
+    <p> I took a remote position with a software development company as a project
         manager and recruiter. They built FAST and there was no real requirements 
         gathering going on. It was not what I was used to but it was great to see 
         how different it can be from one company to another. I learned a lot from 
         a project management perspective. As much as I was learning about the SDLC
         I was not learning about actual coding. 
+    </p>
         
-        That is when I dedided to take one of the biggest leaps in faith that I have 
+    <p> That is when I dedided to take one of the biggest leaps in faith that I have 
         ever taken and quit my job to start a dev bootcamp. I ended up going to LAMP
         camp, and from the name you can guess that it is a full-stack PHP program.
         The only difference is that it is not a dev bootcamp. It is subsidized by 
@@ -58,24 +63,27 @@ $this->params['breadcrumbs'][] = $this->title;
         off by introducing us to their proprietary MVC framework that was built 
         in-house and gave us a lesson in Object Oriented Programming. I can not
         begin to explain how lost I felt at this point. It was law school all over
-        again!
+        again! 
+    </p>
         
-        I won't lie, I thought about quitting about 10 times that week and signing
+    <p> I won't lie, I thought about quitting about 10 times that week and signing
         up for a traditional dev bootcamp. Thank god I didn't because I know now
         how different of an experience it would have been. There is big differnce
         when you are dealing with key business decisions and from my short time at
         LAMP camp I learned how to take requirements, clarify, develop and iterate. 
         That was priceless! 
+    </p>
         
-        I also know now where I want to go with my programming in the future. I 
+    <p> I also know now where I want to go with my programming in the future. I 
         was hired by a tech recruting company recently as a senior recruiter and
         I cannot wait to begin. I wil continue coding in the evening and getting 
         as much freelance work as I can. I want to continue to progress and will 
-        not let anything slow me down. 
+        not let anything slow me down.
+    </p>
         
-        I also want to gain further knowledge of intellectual property law and as
+    <p>    I also want to gain further knowledge of intellectual property law and as
         I see it being a big part of my future. I think some forget how important 
-        the legal aspect of appliction development actually is.
+        the legal aspect of appliction development actually is. </br>
     </p>
 
 </div>
