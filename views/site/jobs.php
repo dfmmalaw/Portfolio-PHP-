221690648fac2d1355015ba51cafeeaec5e74b6c
dfmmalaw <dfmmalaw@gmail.com>
2015-11-29 19:50:59 +0000

    added content to jobs page

--- views/site/jobs.php
+++ views/site/jobs.php
@@ -24,34 +24,34 @@ An ideal candidate should be a fast learner of new tools and technologies. The c
 Position Responsibilities
 </p>
 <ul>
-<li>• Plan functional solutions to complex problems</li>
-<li>• Deliver high-quality and efficient Java-based software solutions</li>
-<li>• Work closely with other departments to ensure all relevant and agreed upon aspects of the application communicate efficiently</li>
-<li>• Follow and help evolve coding standards and practices</li>
-<li>• Collaborate with team members to bridge the gap between front-end and back-end applications</li>
+<li>Plan functional solutions to complex problems</li>
+<li>Deliver high-quality and efficient Java-based software solutions</li>
+<li>Work closely with other departments to ensure all relevant and agreed upon aspects of the application communicate efficiently</li>
+<li>Follow and help evolve coding standards and practices</li>
+<li>Collaborate with team members to bridge the gap between front-end and back-end applications</li>
 </ul>
 <p>
-Skill Requirements
+Requirements
 </p>
 <ul>
-<li>• Bachelor’s degree (or higher) in Computer Science or related field</li>
-<li>• Proficiency in Java with 2+ years of professional experience</li>
-<li>• JSP/JSTL proficiency in an MVC environment</li>
-<li>• Knowledge and experience with Agile development practices</li>
-<li>• Experience and desire to work in a test driven development environment moving toward continuous deployment</li>
-<li>• Reasonable knowledge of NoSQL implementations</li>
-<li>• Experience with Tomcat and Apache</li>
+<li>Bachelor’s degree (or higher) in Computer Science or related field</li>
+<li>Proficiency in Java with 2+ years of professional experience</li>
+<li>JSP/JSTL proficiency in an MVC environment</li>
+<li>Knowledge and experience with Agile development practices</li>
+<li>Experience and desire to work in a test driven development environment moving toward continuous deployment</li>
+<li>Reasonable knowledge of NoSQL implementations</li>
+<li>Experience with Tomcat and Apache</li>
 </ul>
 <p>
 Desired Traits:
 </p>
 <ul>
-<li>• 3+ years experience using Java Frameworks, preferably Spring</li>
-<li>• 3+ years experience developing in a CMS driven environment (Magnolia experience a plus)</li>
-<li>• Knowledge and experience working in a cloud environment</li>
-<li>• Direct experience with MongoDB or another NoSQL implementation</li>
-<li>• Experience instituting test driven development and continuous integration process</li>
-<li>• Experience with Jenkins, IntelliJ, Maven, and Jira.</li>
+<li>3+ years experience using Java Frameworks, preferably Spring</li>
+<li>3+ years experience developing in a CMS driven environment (Magnolia experience a plus)</li>
+<li>Knowledge and experience working in a cloud environment</li>
+<li>Direct experience with MongoDB or another NoSQL implementation</li>
+<li>Experience instituting test driven development and continuous integration process</li>
+<li>Experience with Jenkins, IntelliJ, Maven, and Jira.</li>
 </ul>
 
 <h2>
@@ -67,122 +67,189 @@ We need a Sr Software Engineer to build out their evolving game platform. You mu
 Position Responsibilities
 </p>
 <ul>
-<li>• Work closely with the Product Manager and executive team to develop and enhance Kaneva’s core game platform.</li>
-<li>• Work with all areas of product engineering, as well as game feature development goals and execution.</li>
-<li>• Work with team to architect and design new platform features</li>
-<li>• Create documentation to clearly communicate ideas and goals to team.</li>
-<li>• Spearhead and deliver work on time within a schedule.</li>
+<li>Work closely with the Product Manager and executive team to develop and enhance Kaneva’s core game platform.</li>
+<li>Work with all areas of product engineering, as well as game feature development goals and execution.</li>
+<li>Work with team to architect and design new platform features</li>
+<li>Create documentation to clearly communicate ideas and goals to team.</li>
+<li>Spearhead and deliver work on time within a schedule.</li>
 </ul>
 <p>
-Skill Requirements
+Requirements
 </p>
 <ul>
-<li>• 5+ years of experience as a C++ programmer</li>
-<li>• Strong Computer Science fundamentals in algorithms, design patterns and data structures</li>
-<li>• Advanced software engineering skills, including the ability to write expert-level, maintainable, and robust code in C++</li>
-<li>• Experience with Windows and cross platform C++ development techniques</li>
-<li>• Understanding and passion for the competitive, multiplayer game landscape.</li>
-<li>• Working experience with multithreaded and distributed systems</li>
-<li>• Strong methodology and problem solving ability, with focus on technical design and UI art implementation</li>
-<li>• Undergraduate or graduate degree in Computer Science or related fields</li>
+<li>5+ years of experience as a C++ programmer</li>
+<li>Strong Computer Science fundamentals in algorithms, design patterns and data structures</li>
+<li>Advanced software engineering skills, including the ability to write expert-level, maintainable, and robust code in C++</li>
+<li>Experience with Windows and cross platform C++ development techniques</li>
+<li>Understanding and passion for the competitive, multiplayer game landscape.</li>
+<li>Working experience with multithreaded and distributed systems</li>
+<li>Strong methodology and problem solving ability, with focus on technical design and UI art implementation</li>
+<li>Undergraduate or graduate degree in Computer Science or related fields</li>
 </ul>
 <p>
 Desired Traits:
 </p>
 <ul>
-<li>• Experience working in a fast paced Agile and TDD software delivery environment</li>
-<li>• Ability to take constructive criticism and maintain a professional attitude.</li>
-<li>• Independently self-motivated, team player with excellent communication skills.</li>
-<li>• Experience with scripting languages such as Lua, Javascript, Python, Ruby</li>
-<li>• Experience with other Game Engines such as: Source, Unreal, or CryEngine.</li>
+<li>Experience working in a fast paced Agile and TDD software delivery environment</li>
+<li>Ability to take constructive criticism and maintain a professional attitude.</li>
+<li>Independently self-motivated, team player with excellent communication skills.</li>
+<li>Experience with scripting languages such as Lua, Javascript, Python, Ruby</li>
+<li>Experience with other Game Engines such as: Source, Unreal, or CryEngine.</li>
 </ul>
-</p>
+
 
 <h2>
 Description
 </h2>
 <p>
-
+There are two types of places you can work: the predictable 9-5 job or the kind of experience that inspires and challenges you every day to be better, work smarter, push yourself further than you have ever gone. StrataCloud is a place where we run hard, learn quickly and celebrate winning, because winning is fun. 
 </p>
 <p>
-
+If you have the education and StrataCloud’s work philosophy excites you, then continue reading. In this role, you will drive the development of an award winning multi-million dollar product and be involved with developing key product deliverables including, but not limited to, developing data collection and manipulation, distributed application networking, caching, high speed data analysis, and big-data visualization capabilities.
+</p>
+<p>
+The ideal candidate will have programming skills, be proactive, and must be able to work on multiple projects simultaneously while under tight deadlines. 
 </p>
 <p>
 Position Responsibilities
 </p>
 <ul>
-
+<li>Design, develop, implement, and maintain Enterprise Java applications.</li>
+<li>Assist in the design effort and provide work estimates.</li>
+<li>Design, code, and test software with limited supervision.</li>
+<li>Follow all appropriate design, development, and testing specifications.</li>
+<li>Collaborate with team members and customers to define customer requirements and system interfaces.</li>
 </ul>
 <p>
-Skill Requirements
+Requirements
 </p>
 <ul>
-
+<li>Bachelor’s degree in Computer Science or related IT degree.</li>
+<li>1 - 3 years of Enterprise Java application experience with Java EE 5 or 6.</li>
+<li>Hands-on experience with at least one of the major Java EE Application servers - WebSphere, WebLogic, Glassfish, and JBoss.</li>
+<li>Hands-on experience with Oracle database.</li>
+<li>Java Web Framework, such as Struts, Spring MVC, and JSF.</li>
+<li>Sophisticated SQL queries.</li>
+<li>Ant build.</li>
+<li>Working knowledge of Agile development.</li>
+<li>1-3 years of hands-on experience in the following areas:</li>
+<li>  --> Selenium - Web Browser Automation</li>
+<li>  --> JSP</li>
+<li>  --> Core Java</li>
+<li>  --> Apache Jmeter</li>
 </ul>
-<p>
-Desired Traits:
-</p>
-<ul>
 
-</ul>
-</p>
 
 <h2>
 Description
 </h2>
 <p>
-
+The Application Developer is an individual contributor who is responsible for designing, developing, modifying and supporting packaged and custom software applications that enable MCIC’s internal and external clients in running and performing their Medical Malpractice and Risk Retention business functions. 
 </p>
 <p>
-
-</p>
-<p>
-Position Responsibilities
-</p>
-<ul>
-
-</ul>
-<p>
-Skill Requirements
-</p>
-<ul>
-
-</ul>
-<p>
-Desired Traits:
-</p>
-<ul>
-
-</ul>
-</p>
-
-<h2>
-Description
-</h2>
-<p>
-
-</p>
-<p>
-
+The ideal candidate needs to be quality focused and a strong background in Agile development.  The Application Developer will be part of a delivery team and will need to interact with other team members including BAs, QA’s and offshore resources.
 </p>
 <p>
 Position Responsibilities
 </p>
 <ul>
-
+<li>Analyze business requirements, create detailed designs, develop and test high quality software.</li>
+<li>Enhance existing applications developed in .Net.</li>
+<li>Support, maintain and enhance existing data structures and stored procedures for existing applications.</li>
+<li>Extend & enhance our web based central applications portal & front-end in JavaScript</li>
+<li>Provide developer/level 3 support for our in-house developed and package applications.</li>
+<li>Support, administer, and configure package applications.</li>
+<li>Improve package applications by delivering detailed specifications to the vendor and managing the development, testing, and implementation of those applications.</li>
+<li>Estimate and execute assigned Sprint activities.</li>
+<li>Advocate the migration from a waterfall development methodology to an Agile development/delivery methodology.</li>
+<li>Some legacy applications are built in Coldfusion (Desired).</li>
 </ul>
 <p>
-Skill Requirements
+Requirements
 </p>
 <ul>
-
+<li>Bachelor degree in Computer Science or related field.</li>
+<li>.Net, JavaScript, CSS, HTML</li>
+<li>SQL Server skills with SQL views, functions and stored procedures</li>
+<li>Significant Agile/Scrum experience.</li>
+<li>Minimum of 5-7 years progressive application development experience.</li>
+<li>Strong Object oriented programming skills.</li>
+<li>Willingness to work in and foster a structured development environment</li>
+<li>Willingness to take direction but also offer creative ideas, angles and solutions to enhance impact of communications</li>
+<li>Work closely with team members to develop efficient and high-quality applications</li>
+<li>Keep up to date on industry trends and new technologies</li>
+<li>Desired skills include: SharePoint, force.com, and Coldfusion.</li>
+<li>Experience in the insurance industry is a plus.</li>
 </ul>
 <p>
 Desired Traits:
 </p>
 <ul>
-
+<li>Must be decisive, flexible, and results-oriented with the ability and desire to achieve goals.</li>
+<li>Demonstrated skills in strategic and critical thinking.</li>
+<li>Ability to effectively understand needs and communicate solutions to others.</li>
+<li>Ability to provide exceptional customer service.</li>
+<li>Strong interpersonal skills</li>
+<li>Highly organized</li>
+<li>Focused work ethic</li>
 </ul>
-</p>
 
-</div>
+
+<!--<h2>-->
+<!--Description-->
+<!--</h2>-->
+<!--<p>-->
+
+<!--</p>-->
+<!--<p>-->
+
+<!--</p>-->
+<!--<p>-->
+<!--Position Responsibilities-->
+<!--</p>-->
+<!--<ul>-->
+
+<!--</ul>-->
+<!--<p>-->
+<!--Skill Requirements-->
+<!--</p>-->
+<!--<ul>-->
+
+<!--</ul>-->
+<!--<p>-->
+<!--Desired Traits:-->
+<!--</p>-->
+<!--<ul>-->
+
+<!--</ul>-->
+
+<!--<h2>-->
+<!--Description-->
+<!--</h2>-->
+<!--<p>-->
+
+<!--</p>-->
+<!--<p>-->
+
+<!--</p>-->
+<!--<p>-->
+<!--Position Responsibilities-->
+<!--</p>-->
+<!--<ul>-->
+
+<!--</ul>-->
+<!--<p>-->
+<!--Skill Requirements-->
+<!--</p>-->
+<!--<ul>-->
+
+<!--</ul>-->
+<!--<p>-->
+<!--Desired Traits:-->
+<!--</p>-->
+<!--<ul>-->
+
+<!--</ul>-->
+
+
+<!--</div>-->
