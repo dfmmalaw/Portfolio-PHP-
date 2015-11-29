d12c44330159c67f52106d483a998af012cd66e8
dfmmalaw <dfmmalaw@gmail.com>
2015-11-29 22:14:26 +0000

    added deanfriedland urls to all the other pages

--- views/site/index.php
+++ views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'My Yii Application';
 
         <p class="lead">Follow me on my journey of learning to code...</p>
 
-        <p><a class="btn btn-lg btn-success" href="https://portfolio-php--dfmmalaw.c9.io/basic/web/index.php/site/story">My Story</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.deanfriedland.com/index.php/site/story">My Story</a></p>
     </div>
 
     <div class="body-content">
@@ -22,7 +22,7 @@ $this->title = 'My Yii Application';
 
                 <p>Add coding tips to help others!</p>
 
-                <p><a class="btn btn-default" href="https://portfolio-php--dfmmalaw.c9.io/basic/web/index.php/tip/index">Click Me &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.deanfriedland.com/index.php/tip/index">Click Me &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>My Code</h2>
@@ -36,7 +36,7 @@ $this->title = 'My Yii Application';
 
                 <p>Need a job? View my listings!</p>
 
-                <p><a class="btn btn-default" href="https://portfolio-php--dfmmalaw.c9.io/basic/web/index.php/site/jobs">Click Me &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.deanfriedland.com/index.php/site/jobs">Click Me &raquo;</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>Resources</h2>
