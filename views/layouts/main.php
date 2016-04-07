1d31fcc3f6fc658e41b4c4a696919862bd940d60
dfmmalaw <dfmmalaw@gmail.com>
2016-04-07 04:05:22 +0000

    chaged routing on Home button on header

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,7 +37,7 @@ AppAsset::register($this);
     ]);
     
     $navItems=[
-    ['label' => 'Home', 'url' => ['/']],
+    ['label' => 'Home', 'url' => ['']],
     ['label' => 'About', 'url' => ['/site/about']],
     ['label' => 'Contact', 'url' => ['/site/contact']]
   ];
