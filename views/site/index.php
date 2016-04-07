58e0cb751957040af8d321afb3dee2dcee749066
dfmmalaw <dfmmalaw@gmail.com>
2016-04-07 03:15:01 +0000

    Removed the links from home page

--- views/site/index.php
+++ views/site/index.php
@@ -17,7 +17,7 @@ $this->title = 'My Yii Application';
 
         <p class="lead">Follow me on my journey of learning to code...</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.deanfriedland.com/index.php/site/story">My Story</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.tip-it.tech/index.php/site/story">My Story</a></p>
     </div>
 
     <div class="body-content">
@@ -33,66 +33,9 @@ $this->title = 'My Yii Application';
 
                 <p>Add coding tips to help others!</p>
 
-                <p><a class="btn btn-default" href="http://www.deanfriedland.com/index.php/tip/index">Click Me &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.tip-it.tech/index.php/tip/index">Click Me &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>My Code</h2>
-                
-                <?php echo MD::icon(MD::_CODE)
-                    ->size(MD::SIZE_3X) 
-                    // ->spin();
-                ?>
-
-                <p>Check out what I have done!</p>
-
-                <p><a class="btn btn-default" href="https://github.com/dfmmalaw">Click Me &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>I'm Hiring</h2>
-                
-                <?php echo MD::icon(MD::_WORK)
-                    ->size(MD::SIZE_3X) 
-                    // ->spin();
-                ?>
-
-                <p>Need a job? View my listings!</p>
-
-                <p><a class="btn btn-default" href="http://www.deanfriedland.com/index.php/site/jobs">Click Me &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Resources</h2>
-                
-                <?php echo MD::icon(MD::_SCHOOL)
-                    ->size(MD::SIZE_3X) 
-                    // ->spin();
-                ?>
-
-                <p>Some awesome coding tutorials!</p>
-
-                <p><a class="btn btn-default" href="http://www.labnol.org/internet/learn-coding-online/28537/">Click Me &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>The Law</h2>
-                
-              <i class="material-icons md-gavel">gavel</i>
-
-                <p>Happening in the legal side of tech?</p>
-
-                <p><a class="btn btn-default" href="http://www.lawtechnologytoday.org">Click Me &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                
-                <h2>Events</h2>
-                
-                <?php echo MD::icon(MD::_EVENT)
-                    ->size(MD::SIZE_3X) 
-                    // ->spin();
-                ?>
-              
-                <p>What's going on in the coding world?</p>
-
-                <p><a class="btn btn-default" href="http://www.meetup.com">Click Me &raquo;</a></p>
-            </div>
+            
             
         </div>
 
