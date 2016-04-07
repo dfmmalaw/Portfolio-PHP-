051a6001e7a073c76697e78a7b57e6c868c00ca3
dfmmalaw <dfmmalaw@gmail.com>
2016-04-07 03:54:36 +0000

    added gradient color to background

--- views/site/about.php
+++ views/site/about.php
@@ -11,24 +11,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p> 
-        Hi! My name is Dean Friedland and this site is about me and my journey of 
-        learning how to code. I have an incredible wife (Ashley), daughter (Reese), 
-        and son on the way. There is nothing better than being a husband and father. 
-        I love all things martial arts and will train and compete in Brazilian 
-        Jiu Jitsu until the day I die! I also love to surf and have water in my 
-        blood. I am currently living in Georgia and the closest beach is hours 
-        away which I hate.
-    </p>
-    <p>
-        I am originally from South Africa but have lived in the 
-        US since I was a kid after a six year stint in Canada. I have also lived
-        in both Ecuador and Costa Rica (both of which have killer
-        waves) and am blessed to have traveled to many other countries in this amazing
-        world.  
-    </p>
-    <p>
-        I hope you enjoy my site and please let me know if you would like me to 
-        add any content. Thanks for visiting!
+        Hi! My name is Dean Friedland and I am 
+        learning how to code. I created this basic CRUD application so that devs
+        of all levels can leave tips for those who are also on the journey of learning how to 
+        code. Please pay it forward by adding a piece of knowledge that you wish
+        someone would have told you along the way... Thank you!!!
     </p>
 
 </div>
--- views/site/story.php
+++ views/site/story.php
@@ -4,85 +4,18 @@
 
 use yii\helpers\Html;
 
-$this->title = 'My Story';
+$this->title = 'What is this app???';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-story">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        My story may not be much different from others that fell into the world 
-        of "coding". At 32 I still wasn't sure what I wanted do with the rest of
-        my life, and being a husband, father and provider made this feeling of uncertainty 
-        unbearable. I knew there had to be something that I would be happy doing 
-        on a daily basis and would allow me to be financially secure. Enter 
-        programming! But first some background...
-    </p>
-        
-     <p>During law school I did my clinic at the prosecutor's office in Atlanta, 
-        GA. I absolutely loved being in the court room and because of something 
-        called the Third Year Practice Act I was able to try my own cases. It was 
-        awesome to say the least. In fact, my plan was to take the bar and become 
-        a prosecutor after I graduated. 
-    </p>
-        
-    <p> Fast forward, I graduated from law school and took a job teaching and also as
-        the program chair of a criminal justice program at a local college. I was
-        there for a little over a year until I was offered a position as a Director 
-        of Education at another school. Soon after I realized that management in 
-        education was not what I wanted to do the rest of my life. 
-    </p>
-        
-    <p> I ended up leaving Georgia with my wife and three month old daughter and
-        heading to Florida to take a position with a family member's start-up 
-        software company. I immediately fell in love with the tech industry! Everything
-        from the technical aspects of the software development life cycle to the 
-        legal aspect of intellectual property just seemed to make sense to me. I 
-        had a chance to be a part of the re-architecture of the software starting 
-        at the design and through its eventual implementation. I was also blown
-        away by the designers and developers and what they were able to create. I 
-        knew immediately that I would have to learn how to code if I was ever 
-        going to really understand the technical side of things. 
-    </p>
-        
-    <p> I took a remote position with a software development company as a project
-        manager and recruiter. They built FAST and there was no real requirements 
-        gathering going on. It was not what I was used to but it was great to see 
-        how different it can be from one company to another. I learned a lot from 
-        a project management perspective. As much as I was learning about the SDLC
-        I was not learning about actual coding. 
-    </p>
-        
-    <p> That is when I dedided to take one of the biggest leaps in faith that I have 
-        ever taken and started attending a dev bootcamp. I ended up going to LAMP
-        camp, and from the name you can guess that it is a full-stack PHP program.
-        The difference is that it is not a dev bootcamp per se. It is subsidized by 
-        a software company called The Cresca Group and us Junior Developers get to 
-        work on their client's "enterprise level" web applications. On day one they started 
-        off by introducing us to their proprietary MVC framework that was built 
-        in-house and gave us a crash course in Object Oriented Programming. I cannot
-        begin to explain how lost I felt at this point. It was law school all over
-        again! 
-    </p>
-        
-    <p> I won't lie, I thought about quitting about 10 times that week and signing
-        up for a traditional dev bootcamp. Thank god I didn't because I know now
-        how different of an experience it would have been. There is big differnce
-        when you are dealing with key business decisions and from my short time at
-        LAMP camp I learned how to take requirements, clarify, develop and iterate. 
-        That was priceless! 
-    </p>
-        
-    <p> I also know now where I want to go with my programming in the future. I 
-        was hired by a tech recruting company recently as a senior recruiter and
-        I cannot wait to begin. I wil continue coding in the evening and getting 
-        as much freelance work as I can. I want to continue to progress and will 
-        not let anything slow me down.
-    </p>
-        
-    <p> I also want to gain further knowledge of intellectual property law and as
-        I see it being a big part of my future. I think some forget how important 
-        the legal aspect of appliction development actually is. </br>
+        Hi! My name is Dean Friedland and I am 
+        learning how to code. I created this basic CRUD application so that devs
+        of all levels can leave tips for those who are also on the journey of learning how to 
+        code. Please pay it forward by adding a piece of knowledge that you wish
+        someone would have told you along the way... Thank you!!!
     </p>
 
 </div>
