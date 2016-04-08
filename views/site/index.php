df9353d4381b14dc216bea37c52c93ae621d8f6a
dfmmalaw <dfmmalaw@gmail.com>
2016-04-08 05:04:44 +0000

    Added the correct domain name for the tip feature and link

--- views/site/index.php
+++ views/site/index.php
@@ -13,11 +13,11 @@ $this->title = 'My Yii Application';
         
         <img src="ecto.png" alt="HTML5 Icon" style="width:148px;height:148px;">
         
-        <h1>Tip IT</h1>
+        <h1>The Tip-IT App</h1>
 
         <p class="lead">What do you wish someone told you when you were learning to code???</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://portfolio-php.herokuapp.com/index.php/tip/index">Leave a tip!</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.tip-it.tech//index.php/tip/index">Leave a tip!</a></p>
     </div>
 
     <div class="body-content">
